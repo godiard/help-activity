af35c106a6e2082dcf3bc5a6dcf3d3dc73e07eab
Gonzalo Odiard <godiard@gmail.com>
2013-10-31 18:15:56 -0300

    Modify setup.py to be able to include the html generated content
    Because the html is not included in the git repository,
    we need modify the standard XOPackager.
    
    Signed-off-by: Gonzalo Odiard <gonzalo@laptop.org>

--- setup.py
+++ setup.py
@@ -14,6 +14,79 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import logging
+import os
+import zipfile
+from fnmatch import fnmatch
+
 from sugar3.activity import bundlebuilder
+
+"""
+We reimplement the packager, to add to the .xo file the html files
+because they are not in git
+"""
+
+INCLUDE_DIRS = ['activity', 'html', 'images', 'source', 'locale']
+
+IGNORE_FILES = ['.gitignore', '*.pyc', '*~']
+
+
+class XOHelpPackager(bundlebuilder.Packager):
+
+    def __init__(self, builder):
+        bundlebuilder.Packager.__init__(self, builder.config)
+
+        self.builder = builder
+        self.builder.build_locale()
+        self.package_path = os.path.join(self.config.dist_dir,
+                                         self.config.xo_name)
+
+    def package(self):
+        bundle_zip = zipfile.ZipFile(self.package_path, 'w',
+                                     zipfile.ZIP_DEFLATED)
+
+        for f in self.list_files('./', True):
+            logging.info('Adding %s', f)
+            bundle_zip.write(os.path.join(self.config.source_dir, f),
+                             os.path.join(self.config.bundle_root_dir, f))
+
+        bundle_zip.close()
+
+    def list_files(self, base_dir, filter_directories=False):
+        if filter_directories:
+            include_dirs = INCLUDE_DIRS
+        else:
+            include_dirs = None
+
+        ignore_files = IGNORE_FILES
+        result = []
+
+        base_dir = os.path.abspath(base_dir)
+
+        for root, dirs, files in os.walk(base_dir):
+
+            if ignore_files:
+                for pattern in ignore_files:
+                    files = [f for f in files if not fnmatch(f, pattern)]
+
+            rel_path = root[len(base_dir) + 1:]
+            for f in files:
+                result.append(os.path.join(rel_path, f))
+
+            if root == base_dir:
+                n = 0
+                while n < len(dirs):
+                    directory = dirs[n]
+                    if include_dirs is not None and \
+                            not directory in include_dirs:
+                        logging.debug("** Ignoring directory %s", directory)
+                        dirs.remove(directory)
+                    else:
+                        n = n + 1
+        return result
+
+
+bundlebuilder.XOPackager = XOHelpPackager
+
 
 bundlebuilder.start()
