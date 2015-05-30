fc61a5ec5c09d42b8875d57173598e96c69ba557
Gonzalo Odiard <godiard@gmail.com>
2015-05-30 15:54:01 -0300

    Update Sugar version to 0.104

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2013, SugarLabs'
 # built documents.
 #
 # The short X.Y version.
-version = '0.100'
+version = '0.104'
 # The full version, including alpha/beta/rc tags.
 release = '0.100'
 
