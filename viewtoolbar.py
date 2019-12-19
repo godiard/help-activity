d0f80b55cab8446c8cb2b2e4a1121741068262e1
Creatune <rjjoshixyz@gmail.com>
2019-12-19 03:54:44 +0000

    Port To Python 3

--- viewtoolbar.py
+++ viewtoolbar.py
@@ -15,7 +15,8 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 from gettext import gettext as _
-
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import GObject
 
