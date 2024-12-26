d150f72b93555209cc176fe43d1ddb86bbd17ee2
Chihurumnaya Ibiam <ibiam@sugarlabs.org>
2024-12-26 10:44:55 +0100

    Update Sugar Version
    Signed-off-by: Chihurumnaya Ibiam <ibiam@sugarlabs.org>

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2021, SugarLabs'
 # built documents.
 #
 # The short X.Y version.
-version = '0.118'
+version = '0.121'
 # The full version, including alpha/beta/rc tags.
 release = version
 
